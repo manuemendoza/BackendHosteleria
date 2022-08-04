bbccc1ad643b8c39e2f33e72bb94f0a654ce8ac0
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2022-08-05 00:46:25 +0200

    conecting to the database

---
+++ app/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+app.use(express.json());
+
+app.get('/coffee', (req, res) => res.send('So sorry', 418));
+
+app.listen(process.env.PORT, () => console.log('El servidor esta levantado en ,', process.env.PORT));
